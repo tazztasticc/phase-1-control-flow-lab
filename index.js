56769654944069333b939215be5a09272e1017ec
Tasif <taz181995@gmail.com>
2021-06-30 01:33:15 -0400

    commit tree a3018ba3ace223e06ab42ed66972091ff9cad30f

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(depth){
+  if (depth <= 400) 
+    {return "This one is on me!"}
+  else if (depth <=2499)
+    {return "I will gladly take your thirty bucks."}
+  else (depth >=2500)
+    {return "No can do."}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city == 'NYC')
+    {return "Ok, sounds good."}
+  else 
+    {return "No go."}
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip == 'generous')
+    {return "Thank you so much."}
+  else if (tip == 'not as generous')
+    {return "Thank you."}
+  else 
+    {return "Bye."}
 }
